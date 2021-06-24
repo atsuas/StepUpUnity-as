e397c18b1e39070bf1701b43e1e6444a0cc2f68d
as <>
2021-06-24 09:15:25 +0900

    imageの位置を操作する

---
+++ Assets/Script/UIsample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIsample : MonoBehaviour
+{
+    [SerializeField]
+    private Image _image;
+    [SerializeField]
+    private Text _text;
+    
+    void Start()
+    {
+        _image.transform.localPosition = Vector2.zero;
+
+        ////imageを300×300に変更する
+        //_image.rectTransform.sizeDelta = new Vector2(300f, 300f);
+        ////テキストを変更し、カラーをブルーに変更する
+        //_text.text = "テキスト変更";
+        //_text.color = Color.blue;
+    }
+
+    void Update()
+    {
+        
+    }
+}
